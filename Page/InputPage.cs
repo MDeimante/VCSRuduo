c2bf0abb4145c35e0f951e5613f0732703660bc4
DESKTOP-2ROI8BE\Deimante <deimante.mikenaite@yahoo.com>
2021-09-26 02:02:33 +0300

    koks nors komentaras

---
+++ Page/InputPage.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static NUnit.Core.NUnitFramework;
+
+namespace TestAutomation.Page
+{                           //PAGE OBJECT PATTERN. web pusl traktuojamas kaip klase, o jos laukai - puslapio elementai. klases
+                                //metodai - tai veiksmai puslapyje
+    public class InputPage
+    {
+        public static IWebDriver _driver;
+        //apsirasom kintamuosius t.y. elementus: 
+        private IWebElement _inputField => _driver.FindElement(By.Id("sum1")); //_pradzioje nes elementai globalus siai klasei
+        private IWebElement _showMessageButton => _driver.FindElement(By.CssSelector("#get-input > button"));
+
+        private IWebElement _actualResult => _driver.FindElement(By.Id("display"));
+
+        private IWebElement _firstInput =>  _driver.FindElement(By.Id("sum1"));
+        //private kad kitos klases nenaudotu.> varnele tam kad sio elemeto ieskotu tik tada kai jo reikia
+        private IWebElement _secondInput => _driver.FindElement(By.Id("sum1"));
+
+        private IWebElement _getTotalButton => _driver.FindElement(By.CssSelector("#gettotal > button"));
+
+        private IWebElement _actualSumResult => _driver.FindElement(By.Id("displayvalue"));
+
+        //sukuriam siai klasei konstruktoriu
+        public InputPage(IWebDriver webdriver)
+        {
+            _driver = webdriver; 
+        }
+
+        public void InsertTextToInputField(string text)
+        {
+            _inputField.Clear();
+            _inputField.SendKeys(text);
+        }
+
+        public void ClickShowMessageButton()
+        {
+            _showMessageButton.Click();
+        }
+
+        //arba ivedam iskart abi reiksmes:
+        //public void InsertBothValuesToInputField(string firstValue, string secondValue)
+        /*
+        {
+        InsertTextToTheFirstInputField(firstValue);
+         InsertTextToTheSecondInputField(secondValue);
+        }
+        */
+
+        public void VerifyResult(string result)
+        {
+            Assert.AreEqual(result, _actualResult.Text, "text is different");
+        }
+    }
+}
---
+++ Tests/InputTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAutomation.Page;
+
+namespace TestAutomation.Tests
+{
+    public class InputTest
+    {
+        public static IWebDriver _driver;
+        [OneTimeSetUp] //kas vyksta iki testu
+        public void SetUp() //parasius sia kodo dali uz testo ribu, visiems 3 testams ji bus pritaikyta automatiskai
+        {
+            IWebDriver _driver = new ChromeDriver(); //sita kintamaji iskelsim pries klase globaliai, nes jis naudojamas visur
+            _driver.Manage().Window.Maximize();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.Url = "https://www.seleniumeasy.com/test/basic-first-form-demo.html";
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+
+            IWebElement popup = _driver.FindElement(By.Id("at-cv-lightbox-close"));
+            wait.Until(e => e.FindElement(By.Id("at-cv-lightbox-close")).Displayed;
+            popup.Click();
+        }
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _driver.Close();
+        }
+        //APJUNGIAM TEST IR PAGE folderius: tiesiog atliekam funkcijas:
+        [Test]
+        public static void TestInputField()
+        {
+            InputPage page = new InputPage(_driver);
+            string text = "Penktadienis";
+
+            page.InsertTextToInputField(text);
+            page.ClickShowMessageButton();
+            page.VerifyResult(text);
+        }
+
+
+    }
+}
